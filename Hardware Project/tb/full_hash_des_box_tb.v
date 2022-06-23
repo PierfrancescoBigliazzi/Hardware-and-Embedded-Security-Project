a5743d31b414063744deab8b231e33e9ae43bed7
Pierfrancesco Bigliazzi <105377940+PierfrancescoBigliazzi@users.noreply.github.com>
2022-06-23 12:26:57 +0200

    Update full_hash_des_box_tb.v

--- Hardware Project/tb/full_hash_des_box_tb.v
+++ Hardware Project/tb/full_hash_des_box_tb.v
@@ -30,81 +30,66 @@ module full_hash_des_box_testbench();
 		.M_valid (DUT_M_valid),
 		.message (DUT_message),
 		.counter (DUT_counter),
-		.digest_out (DUT_digest_out),
+		.digest_out(DUT_digest_out),
 		.hash_ready (DUT_hash_ready)
 	);
 	
 	initial begin
 		
-		// test for an empty sequence
-		// the output is computed by the final computation module on the initialization values
 		begin: TEST_ZERO_LENGTH
 			
-			// expected output 
 			localparam expected_digest_empty = 32'h83656fd2;
 
 			@(reset_deassertion);
 			DUT_M_valid = 0;
 
 			$display("TEST_ZERO_LENGTH");
-
-			// setting inputs
 			@ (posedge clk);
 			DUT_counter = 0;
 			DUT_M_valid = 1;
 			
-			// deasserting the handshake variable
 			@ (posedge clk);
 			DUT_M_valid = 0;
 			
 			@ (posedge clk);
 			@ (posedge clk);
 			@ (posedge clk);
-
-			// compare the result
 			$display("Digest of empty sequence : %h", DUT_digest_out);
 			$display("Test result [ %s ] ", expected_digest_empty == DUT_digest_out ? "Successful" : "Failure" );
-
 			$display("TEST_ZERO_LENGTH finished\n");
 			@ (posedge clk);
 
 		end: TEST_ZERO_LENGTH
 
 		
-		// test of a single character input
+
 		begin: TEST_UPPERCASE_A
 			
-			// expected output
 			localparam expected_digest_A = 32'hc087233c;
 
-			$display("TEST_CHAR_A");
 
-			// setting inputs
+			$display("TEST_CHAR_A");
 			@ (posedge clk);
 			DUT_message = "A";
 			DUT_counter = 1;
 			DUT_M_valid = 1;
 			
-			// deasserting the handshake variable
 			@ (posedge clk);
 			DUT_M_valid = 0;
 			
 			@ (posedge clk);
 			@ (posedge clk);
 			@ (posedge clk);
-
-			// compare the result
 			$display("Digest A character : %h", DUT_digest_out);
 			$display("Test result [ %s ] ", expected_digest_A == DUT_digest_out ? "Successful" : "Failure" );
-			
 			$display("TEST_CHAR_A finished\n");
 			@ (posedge clk);
 
 		end: TEST_UPPERCASE_A
-	
+		
 		begin: TEST_SEQUENCE_AB
 			// expected output
-			localparam expected_digest_AB = 32'h0f4bd2d3;
+			localparam expected_digest_AB = 32'h83656fd4;
 
 			$display("TEST_SEQUENCE_AB");
 
@@ -120,6 +105,8 @@ module full_hash_des_box_testbench();
 			// deasserting the handshake variable
 			@ (posedge clk);
 			DUT_M_valid = 0;
+			DUT_counter = 1'bx;
+			DUT_message = "E";
 			
 			@ (posedge clk);
 			@ (posedge clk);
@@ -133,7 +120,105 @@ module full_hash_des_box_testbench();
 			@ (posedge clk);
 
 		end: TEST_SEQUENCE_AB
+		
+		begin: TEST_SAME_MESSAGE_SAME_HASH
+		
+			reg[31:0] first_Hash;
+			// expected output
+			localparam expected_digest_AB = 32'h83656fd4;
 
+			$display("TEST_SAME_MESSAGE_SAME_HASH");
+
+			// setting inputs
+			@ (posedge clk);
+			DUT_message = "A";
+			DUT_counter = 2;
+			DUT_M_valid = 1;
+
+			@ (posedge clk);
+			DUT_message = "B";
+			
+			// deasserting the handshake variable
+			@ (posedge clk);
+			DUT_M_valid = 0;
+			DUT_counter = 1'bx;
+			DUT_message = "E";
+			
+			@ (posedge clk);
+			@ (posedge clk);
+			@ (posedge clk);
+
+			// compare the result
+			$display("First digest AB sequence : %h", DUT_digest_out);
+			first_Hash = DUT_digest_out;
+			
+			@ (posedge clk);
+			@ (posedge clk);
+			DUT_message = "A";
+			DUT_counter = 2;
+			DUT_M_valid = 1;
+
+			@ (posedge clk);
+			DUT_message = "B";
+			
+			// deasserting the handshake variable
+			@ (posedge clk);
+			DUT_M_valid = 0;
+			DUT_counter = 1'bx;
+			DUT_message = "E";
+			
+			@ (posedge clk);
+			@ (posedge clk);
+			@ (posedge clk);
+
+			// compare the result
+			$display("Second digest AB sequence : %h", DUT_digest_out);
+			$display("Test result [ %s ] ", first_Hash == DUT_digest_out ? "Successful" : "Failure" );
+			$display("TEST_SAME_MESSAGE_SAME_HASH finished\n");
+			
+		end: TEST_SAME_MESSAGE_SAME_HASH
+			
+			
+		
+		begin: TEST_SEQUENCE_A_CLK_B
+			// expected output
+			localparam expected_digest_AB = 32'h83656fd4;
+
+			$display("TEST_SEQUENCE_AB");
+
+			// setting inputs
+			@ (posedge clk);
+			DUT_message = "A";
+			DUT_counter = 2;
+			DUT_M_valid = 1;
+			@ (posedge clk);
+			DUT_M_valid = 0;
+			@ (posedge clk);
+
+			DUT_message = "B";
+			DUT_M_valid = 1;
+			
+			// deasserting the handshake variable
+			@ (posedge clk);
+			DUT_M_valid = 0;
+			
+			@ (posedge clk);
+			@ (posedge clk);
+			@ (posedge clk);
+
+			// compare the result
+			$display("Digest A_CLK_B sequence : %h", DUT_digest_out);
+			$display("Test result [ %s ] ", expected_digest_AB == DUT_digest_out ? "Successful" : "Failure" );
+			
+			$display("TEST_SEQUENCE_A_CLK_BS finished\n");
+			@ (posedge clk);
+
+		end: TEST_SEQUENCE_A_CLK_B
+		
+		
+
+
+	
 		$stop;
 		
 	end
@@ -144,5 +229,4 @@ endmodule
 		
 	
 	
-
 
